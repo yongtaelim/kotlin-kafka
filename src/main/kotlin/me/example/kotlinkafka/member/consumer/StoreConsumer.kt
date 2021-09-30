df734738a383ccfd873b62c1b4de80a159ce64ee
yongtaelim <lyt032410@gmail.com>
2021-09-30 16:11:56 +0900

    code refectoring

--- src/main/kotlin/me/example/kotlinkafka/member/consumer/StoreConsumer.kt
+++ src/main/kotlin/me/example/kotlinkafka/member/consumer/StoreConsumer.kt
@@ -1,32 +1,32 @@
-package me.example.kotlinkafka.member.consumer
-
-import org.apache.kafka.clients.consumer.ConsumerRecord
-import org.apache.logging.log4j.LogManager
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.kafka.annotation.KafkaListener
-import org.springframework.kafka.listener.AcknowledgingMessageListener
-import org.springframework.kafka.support.Acknowledgment
-import org.springframework.stereotype.Component
-
-/**
- * Created by LYT to 2021/08/18
- */
-@Component
-@ConditionalOnProperty(value = ["spring.kafka.type"], havingValue = "routing")
-class StoreConsumer: AcknowledgingMessageListener<String, String> {
-
-    private val log = LogManager.getLogger()
-
-    @KafkaListener(topics = ["one", "two"], groupId = "test", containerFactory = "saveMemberKafkaListener")
-    override fun onMessage(data: ConsumerRecord<String, String>, acknowledgment: Acknowledgment?) {
-        try {
-            log.info("Member Save Consumer Message:: [ $data ] value:: [ ${data.value()} ]")
-
-            // ack 처리
-            acknowledgment?.acknowledge()
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-    }
-
-}
+//package me.example.kotlinkafka.member.consumer
+//
+//import org.apache.kafka.clients.consumer.ConsumerRecord
+//import org.apache.logging.log4j.LogManager
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+//import org.springframework.kafka.annotation.KafkaListener
+//import org.springframework.kafka.listener.AcknowledgingMessageListener
+//import org.springframework.kafka.support.Acknowledgment
+//import org.springframework.stereotype.Component
+//
+///**
+// * Created by LYT to 2021/08/18
+// */
+//@Component
+//@ConditionalOnProperty(value = ["spring.kafka.type"], havingValue = "routing")
+//class StoreConsumer: AcknowledgingMessageListener<String, String> {
+//
+//    private val log = LogManager.getLogger()
+//
+//    @KafkaListener(topics = ["one", "two"], groupId = "test", containerFactory = "saveMemberKafkaListener")
+//    override fun onMessage(data: ConsumerRecord<String, String>, acknowledgment: Acknowledgment?) {
+//        try {
+//            log.info("Member Save Consumer Message:: [ $data ] value:: [ ${data.value()} ]")
+//
+//            // ack 처리
+//            acknowledgment?.acknowledge()
+//        } catch (e: Exception) {
+//            e.printStackTrace()
+//        }
+//    }
+//
+//}
